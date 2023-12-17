676a063988b106a7781de32d35914da4be1d13a7
David Fashola <davidprograms7@gmsil.com>
2023-12-17 12:04:37 -0500

    E-commerstypescript learning

---
+++ 01-helloworld/myhelloworld.js
@@ -0,0 +1,2 @@
+console.log("Hi");
+console.log("Codding ts is " + "Fun for people ");
---
+++ 01-helloworld/myhelloworld.ts
@@ -0,0 +1,6 @@
+console.log("Hi");
+console.log("Codding  is " + "Fun for people ");
+let found: boolean =true;
+let grader: number = 88.6;
+let firstname:String='David';  
+let lastname: String = 'Fashola';
