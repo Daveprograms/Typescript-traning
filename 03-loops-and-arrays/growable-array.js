257ab81cfc894afd23cdd58a9fc3237f8e82e42a
David Fashola <davidprograms7@gmsil.com>
2023-12-17 22:01:47 -0500

    changes

---
+++ 03-loops-and-arrays/growable-array.js
@@ -0,0 +1,4 @@
+var sportsTwo = ["Golf", "Cricket", "Basketball"];
+sportsTwo.push("Basketball");
+sportsTwo.push("Golf");
+sportsTwo.push("hahs");
---
+++ 03-loops-and-arrays/growable-array.ts
@@ -0,0 +1,5 @@
+let sportsTwo : string[]=["Golf", "Cricket", "Basketball"];
+
+sportsTwo.push("Basketball");
+sportsTwo.push("Golf");
+sportsTwo.push("hahs");
