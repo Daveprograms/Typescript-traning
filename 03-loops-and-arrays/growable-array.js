23ee1aacc4ca695666c6d575b03f2f47a1109044
David Fashola <davidprograms7@gmsil.com>
2023-12-17 23:57:52 -0500

    Learning-alot

--- 03-loops-and-arrays/growable-array.js
+++ 03-loops-and-arrays/growable-array.js
@@ -2,3 +2,7 @@ var sportsTwo = ["Golf", "Cricket", "Basketball"];
 sportsTwo.push("Basketball");
 sportsTwo.push("Golf");
 sportsTwo.push("hahs");
+for (var _i = 0, sportsTwo_1 = sportsTwo; _i < sportsTwo_1.length; _i++) {
+    var tempSport = sportsTwo_1[_i];
+    console.log(tempSport);
+}
--- 03-loops-and-arrays/growable-array.ts
+++ 03-loops-and-arrays/growable-array.ts
@@ -1,5 +1,11 @@
-let sportsTwo : string[]=["Golf", "Cricket", "Basketball"];
+// let sportsTwo : string[]=["Golf", "Cricket", "Basketball"];
 
-sportsTwo.push("Basketball");
-sportsTwo.push("Golf");
-sportsTwo.push("hahs");
+// sportsTwo.push("Basketball");
+// sportsTwo.push("Golf");
+// sportsTwo.push("hahs");
+
+// for(let tempSport of sportsTwo){
+//     console.log(tempSport)
+// }
+
+
