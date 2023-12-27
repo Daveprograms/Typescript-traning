b9e0c2ac218ac08e9996bb8456922ec011ba1737
David Fashola <davidprograms7@gmsil.com>
2023-12-27 17:36:49 -0500

    learned a lot

--- 05-inheritance/shape.ts
+++ 05-inheritance/shape.ts
@@ -1,4 +1,25 @@
 export class shape{
-    private_x: number;
-    private_y: number;
+    private _x: any;
+    private _y: any;
+    constructor(private_x: number,private_y: number ){
+        
+    }
+    
+    public get x(): number {
+        return this._x;
+    }
+    public set x(value: number) {
+        this._x = value;
+    }
+    
+
+    public get y(): number {
+        return this._y;
+    }
+    public set y(value: number) {
+        this._y = value;
+    }
+    getInfo():string{
+        return  `x=${this._x}, y=${this._y}`;
+    }
 }
